ceb1092910a7244ef6722e3d419afdac82c24a0c
prasadreddypuli <vtu25749@veltech.edu.in>
2026-06-09 22:17:21 +0530

    Time: 5 ms (37.26%), Space: 44.8 MB (18.2%) - LeetHub

---
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String removeOuterParentheses(String s) {
+        StringBuilder sb = new StringBuilder();
+        int count = 0;
+        for(Character ch:s.toCharArray()){
+            if(ch=='('){
+                if(count>0){
+                    sb.append(ch);
+                }
+                count++;
+            }else{
+                count--;
+                if(count>0){
+                    sb.append(ch);
+                }
+            }
+        }
+        return sb.toString();
+    }
+}
